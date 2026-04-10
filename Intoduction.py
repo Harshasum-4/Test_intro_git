8d3d35fa065aa6d13347d707ce1542775f9d4daf
Harshasum-4 <kukkadapuharsha123@gmail.com>
2026-04-10 08:14:36 +0530

    adding files

--- Intoduction.py
+++ Intoduction.py
@@ -1,3 +1,19 @@
-import pandas as pd
-print("Hello world")
-print("python")
+#Task 1:
+#========
+Name = "Harshavardhan"
+Age = 29
+Height =  5.9
+is_student = False
+print (Name ,Age,Height,is_student)
+
+#Task :2
+#========
+print (" Name: ", Name , " Age: ", Age," Height: ",Height," Student: ",is_student) 
+
+#Task : 3
+#==========
+#Age Calculation
+print ("Age in months:", Age*12)
+print("Age in days:", Age*365)
+print("The remainder when age is divided by 7:", Age%7)
+print("Age is raised to the power of 2:", Age**2)
